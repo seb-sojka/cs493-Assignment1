83a3043a91a4f71fd3e9bf6467dd4311316e0f20
seb-sojka <seb.sojka@gmail.com>
2019-09-28 14:36:58 -0500

    Updated json and js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require(`path`);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 	res.send(`Hello from the otherside`);
 });
@@ -7,4 +11,18 @@ app.get('/', (req, res) => {
 const PORT = process.env.port || 8080;
 app.listen(PORT, () => {
 console.log(`Server listening to smooth jazz on ${PORT} ...`);
+});
+
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    city: req.body.city
+  });
+  var name = req.body.name;
+  var city = req.body.city;
+  res.send(`Hello ${name} from ${city}. How are you today?`);
 });
